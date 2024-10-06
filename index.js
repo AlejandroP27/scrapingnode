4b61db5ee8c9c91f9976d1d939c58c6f7b8f28c5
AlejandroP27 <alejandroperezb27@gmail.com>
2024-10-06 15:54:48 +0200

    cors

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { chromium } = require('playwright');
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    methods: ['*'],
+    allowedHeaders: ['*']
+}));
 
 app.get('/scrape', async (req, res) => {
     try {
