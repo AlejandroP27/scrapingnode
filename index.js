0b333859946eb643ea3c85ca686b5f3789a0f255
AlejandroP27 <alejandroperezb27@gmail.com>
2024-10-19 17:41:23 +0200

    gg

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 app.use(cors({
     origin: '*',
     credentials: true,
@@ -74,107 +76,77 @@ app.use(cors({
 }));
 
 app.get('/scrape', async (req, res) => {
-    
     try {
-            // Lanzar el navegador
-            const browser = await chromium.launch({ headless: true });
-            const userAgentStrings = [
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-            ];
-            const context = await browser.newContext({
-                userAgent: userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)],
-            });
+        // Lanzar el navegador
+        const browser = await chromium.launch({ headless: true });
+        const userAgentStrings = [
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+          ];
+          const context = await browser.newContext({
+            userAgent: userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)],
+          });
+        
+          const page = await context.newPage();
+
             
-            const page = await context.newPage();
-
-           /*  await page.goto('https://www.riamoneytransfer.com/es-es/');
-                
-            try {
-                await page.waitForSelector('div.sc-17effe74-2.ePiGDc', { timeout: 5000 });
-
-                // Hacer clic en el botón con el texto "Rechazar cookies" dentro de ese div
-                await page.click('div.sc-17effe74-2.ePiGDc >> text="Rechazar cookies"');
+            // Close the browser
+        
+        /* const page = await browser.newPage(); */
+        
+        // Navegar a la página objetivo
+        /* await page.goto('https://www.riamoneytransfer.com/es-es/send-money-to-bolivia'); */
+        /* const contenido = await page.evaluate(() => {
             
-                console.log('Botón "Rechazar cookies" clicado correctamente.');
-            } catch (error) {
-                console.log('No se encontró el popup de cookies o ya fue aceptado.');
-            } */
-
-            /* try{ */
+            var cookieBtn = document.querySelector('button.sc-17effe74-7.ecXopu')
+            console.log("ho")
+            console.log(cookieBtn)
+            if(cookieBtn){
+                cookieBtn.click();
+                }
+                }); */     
                 await page.goto('https://www.riamoneytransfer.com/es-es/');
                 
-                try {
-                    await page.waitForSelector('div.sc-17effe74-2.ePiGDc', { timeout: 5000 });
+  try {
+    await page.waitForSelector('div.sc-17effe74-2.ePiGDc', { timeout: 5000 });
 
-                    // Hacer clic en el botón con el texto "Rechazar cookies" dentro de ese div
-                    await page.click('div.sc-17effe74-2.ePiGDc >> text="Rechazar cookies"');
-                
-                    console.log('Botón "Rechazar cookies" clicado correctamente.');
-                } catch (error) {
-                    console.log('No se encontró el popup de cookies o ya fue aceptado.');
-                }
-                /* await page.click('.sc-17effe74-7.ecXopu'); */
-                await page.click('.dropdown-container');
-                await page.waitForTimeout(1000);
-                await page.waitForSelector('.list-search-list'); 
-                await page.click('.list-search-list li:nth-child(26)');
-                // Esperar hasta que se cargue el contenido dinámico
-                await page.waitForTimeout(1000);
+    // Hacer clic en el botón con el texto "Rechazar cookies" dentro de ese div
+    await page.click('div.sc-17effe74-2.ePiGDc >> text="Rechazar cookies"');
+  
+    console.log('Botón "Rechazar cookies" clicado correctamente.');
+  } catch (error) {
+    console.log('No se encontró el popup de cookies o ya fue aceptado.');
+  }
 
-                await page.waitForSelector('.text-promo-rate'); // Cambia el selector por el adecuado
-                // Extraer el contenido dinámico
-                const dynamicContent = await page.evaluate(() => {
-                    return document.querySelector('.text-promo-rate').innerText; // Cambia el selector
-                });
-                context.close();
-                
-                for (let i = 0; i < page.length; i++) {
-                await page[i].close();
-                }
-                // Cerrar el navegador
-                await browser.close();
-                // Enviar la respuesta con el contenido dinámico
-                res.json({ data: dynamicContent });
-            /* } 
-            catch(e){
-                if (e) {
-        
-                // Navegar a la página objetivo
-                    await page.goto('https://www.riamoneytransfer.com/es-es/send-money-to-bolivia');
 
-                    // Esperar hasta que se cargue el contenido dinámico
-                    try {
-                        await page.waitForSelector('div.sc-17effe74-2.ePiGDc', { timeout: 5000 });
+        /* await page.click('.sc-17effe74-7.ecXopu'); */
+        await page.click('.dropdown-container');
+        await page.waitForTimeout(1000);
+        await page.waitForSelector('.list-search-list'); 
+        await page.click('.list-search-list li:nth-child(26)');
+        // Esperar hasta que se cargue el contenido dinámico
+        await page.waitForTimeout(1000);
 
-                        // Hacer clic en el botón con el texto "Rechazar cookies" dentro de ese div
-                        await page.click('div.sc-17effe74-2.ePiGDc >> text="Rechazar cookies"');
-                    
-                        console.log('Botón "Rechazar cookies" clicado correctamente.');
-                    } catch (error) {
-                        console.log('No se encontró el popup de cookies o ya fue aceptado.');
-                    }
+        await page.waitForSelector('.text-promo-rate'); // Cambia el selector por el adecuado
+        // Extraer el contenido dinámico
+        const dynamicContent = await page.evaluate(() => {
+            return document.querySelector('.text-promo-rate').innerText; // Cambia el selector
+        });
 
-                    await page.waitForSelector('.text-promo-rate'); // Cambia el selector por el adecuado
-                    // Extraer el contenido dinámico
-                    const dynamicContent = await page.evaluate(() => {
-                        return document.querySelector('.text-promo-rate').innerText; // Cambia el selector
-                    });
-        
-                    context.close();
-                    
-                    for (let i = 0; i < page.length; i++) {
-                    await page[i].close();
-                    }
-                    // Cerrar el navegador
-                    await browser.close();
-                    // Enviar la respuesta con el contenido dinámico
-                    res.json({ data: dynamicContent });
-                }
-            } */
-        
+        /* const childProcess = browser.process()
+        if (childProcess) {
+        childProcess.kill(9)
+        } */
+        for (let i = 0; i < page.length; i++) {
+          await page[i].close();
+        }
+        // Cerrar el navegador
+        await browser.close();
+
+        // Enviar la respuesta con el contenido dinámico
+        res.json({ data: dynamicContent });
     } catch (error) {
         console.error('Error en el scraping:', error);
         res.status(500).json({ error: 'Error al hacer scraping de la página' });
